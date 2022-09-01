Fix alphas computation

diffdrr/projectors/siddon.py
@@ -34,16 +34,25 @@ class Siddon:
         self.maxidx = ((nx - 1) * (ny - 1) * (nz - 1)).int().item() - 1
 
         # Get the alpha at each plane intersection
-        sx, sy, sz = source
-        alphax = torch.arange(nx, dtype=torch.float32, device=self.device) * dx - sx
-        alphay = torch.arange(ny, dtype=torch.float32, device=self.device) * dy - sy
-        alphaz = torch.arange(nz, dtype=torch.float32, device=self.device) * dz - sz
+        sx, sy, sz = source[:, 0, 0, 0], source[:, 0, 0, 1], source[:, 0, 0, 2]
+        alphax = torch.arange(nx, dtype=torch.float32, device=self.device).unsqueeze(1)
+        alphay = torch.arange(ny, dtype=torch.float32, device=self.device).unsqueeze(1)
+        alphaz = torch.arange(nz, dtype=torch.float32, device=self.device).unsqueeze(1)
+        alphax = (alphax * dx - sx).unsqueeze(-1).unsqueeze(-1).permute(1, 0, 2, 3)
+        alphay = (alphay * dy - sy).unsqueeze(-1).unsqueeze(-1).permute(1, 0, 2, 3)
+        alphaz = (alphaz * dz - sz).unsqueeze(-1).unsqueeze(-1).permute(1, 0, 2, 3)
 
-        sdd = target - source + self.eps  # source-to-detector distance
-        alphax = alphax.unsqueeze(-1).unsqueeze(-1) / sdd[:, :, 0]
-        alphay = alphay.unsqueeze(-1).unsqueeze(-1) / sdd[:, :, 1]
-        alphaz = alphaz.unsqueeze(-1).unsqueeze(-1) / sdd[:, :, 2]
+        sdd = (target - source + self.eps).unsqueeze(1)  # source-to-detector distance
+        alphax = alphax / sdd[:, :, :, :, 0]
+        alphay = alphay / sdd[:, :, :, :, 1]
+        alphaz = alphaz / sdd[:, :, :, :, 2]
+        alphas = torch.cat([alphax, alphay, alphaz], dim=1)
+        delete_tensor(sdd)
+
         alphas = torch.vstack([alphax, alphay, alphaz])
+        delete_tensor(alphax)
+        delete_tensor(alphay)
+        delete_tensor(alphaz)
 
         # Get the alphas within the range [alphamin, alphamax]
         alphamin, alphamax = self.get_alpha_minmax(source, target)